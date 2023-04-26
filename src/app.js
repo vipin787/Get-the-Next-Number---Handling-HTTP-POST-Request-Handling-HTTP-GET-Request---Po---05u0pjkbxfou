33b0fedfe6f0b5e68d04aa0b966e562deab06c70
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2023-04-26 11:41:42 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+//Middlewares
+app.use(express.json());
+
+// Write a GET Request to get the next number from the input 'num'.
+// Endpoint : /api/get-next-num
+// Return the response as {message : , status: }
+
+module.exports = app;
